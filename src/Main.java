82d8fc206ef4276f0292286ba5471b99cddc8ef1
BT_1N3_08 <destinh2@nycstudents.net>
2025-01-10 10:41:54 -0500

    Modified the first print statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("hi Destin Huang");
+        System.out.println("hi brandon clone");
         System.out.println("Hello brandon");
         System.out.println("skibidi toilet sigma ohio rizz kai cenat gyatt");
         System.out.println("hello who ever is reading this");

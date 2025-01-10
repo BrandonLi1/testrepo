02aaa25e544604daee1e01fbff21faf42777ba47
BT_1N3_07 <brandonjl@nycstudents.net>
2025-01-10 10:40:11 -0500

    add method to class

--- src/Sigma.java
+++ src/Sigma.java
@@ -1,6 +1,12 @@
 public class Sigma {
     private String string = "";
+
     public Sigma(){
         string = "hello";
     }
+
+    public static void numba(int n) {
+        System.out.println(n);
+    }
+
 }
